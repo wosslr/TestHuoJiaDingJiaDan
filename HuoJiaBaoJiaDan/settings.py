2ea12834222c6cccc97a9e7bf76e98521add0a58
wosslr <wosslr@163.com>
2015-12-10 02:13:59 +0800

    v1

--- HuoJiaBaoJiaDan/settings.py
+++ HuoJiaBaoJiaDan/settings.py
@@ -77,9 +77,15 @@ WSGI_APPLICATION = 'HuoJiaBaoJiaDan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bjd',
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
