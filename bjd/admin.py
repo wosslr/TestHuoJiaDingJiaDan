fe1d62bb30186f7ab211fd6cfba78ce17df9f7f0
Ray Lv <ray.lv@sap.com>
2015-12-16 17:25:30 +0800

    v3

--- bjd/admin.py
+++ bjd/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, ProductBasePhoto, ProductStyle, ProductPresetStyle, ProductStyleItem
+from .models import Product, ProductBasePhoto, ProductStyle, ProductPresetStyle, ProductStyleItem, ProductPresetStyleItem
+from .models import QuotationHeader, QuotationItem
 
 # Register your models here.
 
@@ -23,6 +24,8 @@ class ProductStyleItemInline(admin.TabularInline):
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
         if db_field.name == 'base_photo':
             product_style_id = request.path.split('/')[-2]
+            print(request.path)
+            print(product_style_id)
             try:
                 product_style = ProductStyle.objects.filter(id=product_style_id)[0]
                 kwargs['queryset'] = ProductBasePhoto.objects.filter(product_id=product_style.product.id)
@@ -38,6 +41,27 @@ class ProductPresetStyleInline(admin.TabularInline):
     extra = 0
 
 
+class ProductPresetStyleItemInline(admin.TabularInline):
+    model = ProductPresetStyleItem
+    extra = 0
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'product_style':
+            product_preset_style_id = request.path.split('/')[-2]
+            try:
+                product_preset_style = ProductPresetStyle.objects.filter(id=product_preset_style_id)[0]
+                kwargs['queryset'] = ProductStyle.objects.filter(product_id=product_preset_style.product.id)
+            except Exception as e:
+                print(e)
+                pass
+        return super(ProductPresetStyleItemInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class ProductPresetStyleAdmin(admin.ModelAdmin):
+    list_display = ['product_preset_style_name', 'product']
+    inlines = [ProductPresetStyleItemInline]
+
+
 class ProductStyleAdmin(admin.ModelAdmin):
     list_display = ['product', 'style_name', 'style_part_name']
     list_display_links = ['style_name']
@@ -50,6 +74,21 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_number', 'product_specification', 'product_category', 'product_technical_data', 'unit_price', 'short_text')
     search_fields = ['product_number', 'product_specification']
 
+
+class QuotationItemInline(admin.TabularInline):
+    model = QuotationItem
+    extra = 0
+    readonly_fields = ['get_product_number', 'get_product_category']
+    raw_id_fields = ['product']
+    # fields = ['product', 'quantity', 'get_product_number', 'get_product_category', 'short_text']
+
+
+class QuotationHeaderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'creation_date', 'creator', 'total_amount', 'short_text']
+    inlines = [QuotationItemInline]
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductStyle, ProductStyleAdmin)
+admin.site.register(ProductPresetStyle, ProductPresetStyleAdmin)
+admin.site.register(QuotationHeader, QuotationHeaderAdmin)
 # admin.site.register(ProductBasePhoto)
--- bjd/models.py
+++ bjd/models.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,7 +22,7 @@ class Product(models.Model):
 
 
 class ProductBasePhoto(models.Model):
-    product = models.ForeignKey(Product)
+    product = models.ForeignKey(Product, verbose_name='产品')
     photo_name = models.CharField(max_length=50, verbose_name='图片描述')
     photo_data = models.ImageField(upload_to='base_photo', verbose_name='底图')
 
@@ -30,27 +31,27 @@ class ProductBasePhoto(models.Model):
 
     class Meta:
         verbose_name_plural = verbose_name = '产品底图'
+        unique_together = ('product', 'photo_name')
 
 
 class ProductStyle(models.Model):
-    product = models.ForeignKey(Product)
+    product = models.ForeignKey(Product, verbose_name='产品')
     # product_base_photo = models.ForeignKey(ProductBasePhoto, verbose_name='产品底图')
     style_part_name = models.CharField(max_length=50, verbose_name='变色部分')
     style_name = models.CharField(max_length=50, verbose_name='样式颜色')
     style_color_code = models.CharField(max_length=100, verbose_name='颜色代码')
 
-
     def __str__(self):
         return self.style_name
 
     class Meta:
-        verbose_name_plural = verbose_name = '产品样式图片'
+        verbose_name_plural = verbose_name = '产品样式'
 
 
 class ProductStyleItem(models.Model):
-    product_style = models.ForeignKey(ProductStyle)
+    product_style = models.ForeignKey(ProductStyle, verbose_name='产品样式')
     # base_photo_name = models.CharField(max_length=50, verbose_name='图片描述')
-    base_photo = models.ForeignKey(ProductBasePhoto)
+    base_photo = models.ForeignKey(ProductBasePhoto, verbose_name='底图')
     style_photo_data = models.ImageField(upload_to='style_photo', verbose_name='样式图片')
 
     def get_product_id(self):
@@ -58,9 +59,47 @@ class ProductStyleItem(models.Model):
 
 
 class ProductPresetStyle(models.Model):
-    product = models.ForeignKey(Product)
-    product_style = models.ManyToManyField(ProductStyle)
+    product = models.ForeignKey(Product, verbose_name='产品')
     product_preset_style_name = models.CharField(max_length=100, verbose_name='预设样式名称')
+
+    class Meta:
+        verbose_name_plural = verbose_name = '产品预设样式'
+
+
+class ProductPresetStyleItem(models.Model):
+    product_preset_style = models.ForeignKey(ProductPresetStyle, verbose_name='产品预设样式')
+    product_style = models.ForeignKey(ProductStyle, verbose_name='产品样式')
+
+
+class QuotationHeader(models.Model):
+    creation_date = models.DateTimeField()
+    creator = models.ForeignKey(User)
+    total_amount = models.DecimalField(max_digits =10, decimal_places=2, default=0)
+    short_text = models.TextField(max_length=500, blank=True, verbose_name='备注')
+
+
+class QuotationItem(models.Model):
+    quotation_header = models.ForeignKey(QuotationHeader)
+    product = models.ForeignKey(Product)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    short_text = models.TextField(max_length=500, blank=True, verbose_name='备注')
+    product_style = models.ManyToManyField(ProductStyle)
+
+    def get_product_number(self):
+        return self.product.product_number
+
+    def get_product_category(self):
+        return self.product.product_category
+
+    get_product_number.allow_tags = True
+    get_product_category.allow_tags = True
+    get_product_category.short_description = '产品类型'
+
+
+
+
+
+
 
 
 
